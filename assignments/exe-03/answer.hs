15ae8a19cf2480b1289f7e102ea4a2e2f5662a74
bidinpithecus <vinibidin@gmail.com>
2024-04-14 17:37:48 -0300

    Huffman coding done. Now need to handle IO

--- assignments/exe-03/answer.hs
+++ assignments/exe-03/answer.hs
@@ -21,14 +21,11 @@ freq [] = []
 freq (x : xs) = Leaf (length l1 + 1) x : freq l2
   where (l1, l2) = partition (== x) xs
 
-sortHuffman :: [Huffman] -> [Huffman]
-sortHuffman = sort
-
 -- Declare uma função que dada a lista com a frequência de cada caratere e retorne a árvore de Huffman
 makeTree :: [Huffman] -> Huffman
 makeTree [] = error "Empty list"
 makeTree [h] = h
-makeTree (h0 : h1 : hs) = makeTree (sortHuffman (merge h0 h1 : hs))
+makeTree (h0 : h1 : hs) = makeTree (sort (merge h0 h1 : hs))
 
 merge :: Huffman -> Huffman -> Huffman
 merge h0 h1 = Node h0 (sumFreqs h0 h1) h1
@@ -40,17 +37,44 @@ weight (Node _ w _) = w
 sumFreqs :: Huffman -> Huffman -> Int
 sumFreqs h0 h1 = weight h0 + weight h1
 
--- -- Declare uma função que dada a árvore de Huffman retorne os códigos de Huffman para cada caractere
--- codeHuffman :: Huffman -> [(Char, String)]
+-- Declare uma função que dada a árvore de Huffman retorne os códigos de Huffman para cada caractere
+codeHuffman :: Huffman -> [(Char, String)]
+codeHuffman h = codeHuffmanHelper h ""
 
+codeHuffmanHelper :: Huffman -> String -> [(Char, String)]
+codeHuffmanHelper (Leaf _ c) code = [(c, code)]
+codeHuffmanHelper (Node l _ r) code = codeHuffmanHelper l (code ++ "0") ++ codeHuffmanHelper r (code ++ "1")
 
--- -- Declare uma função que codifique uma String numa sequência binária de códigos de Huffman
--- encode :: String -> Huffman -> String
+-- Declare uma função que codifique uma String numa sequência binária de códigos de Huffman
+encode :: String -> Huffman -> String
+encode s h = encodeHelper s (codeHuffman h)
 
+encodeHelper :: String -> [(Char, String)] -> String
+encodeHelper [] _ = []
+encodeHelper (c : cs) tuples = case findTupleByFst tuples c of
+  Just (_, code) -> code ++ encodeHelper cs tuples
+  Nothing -> error "Character not found in Huffman encoding table"
 
--- -- Declare uma função que faça o processo inverso que a função `encode`
--- decode :: String -> Huffman -> String
+findTupleByFst :: Eq a => [(a, b)] -> a -> Maybe (a, b)
+findTupleByFst [] elem = Nothing
+findTupleByFst ((x, y) : xs) elem
+  | x == elem  = Just (x, y)
+  | otherwise  = findTupleByFst xs elem
 
+-- Declare uma função que faça o processo inverso que a função `encode`
+decode :: String -> Huffman -> String
+decode [] _ = []
+decode s h =
+  let (ch, rest) = traverseHuffmanTree s h
+  in ch : decode rest h
+
+traverseHuffmanTree :: String -> Huffman -> (Char, String)
+traverseHuffmanTree [] (Leaf _ ch) = (ch, [])
+traverseHuffmanTree s (Leaf _ ch) = (ch, s)
+traverseHuffmanTree (c : cs) (Node l _ r)
+  | c == '0' = traverseHuffmanTree cs l
+  | c == '1' = traverseHuffmanTree cs r
+  | otherwise = error "Invalid Huffman encoding"
 
 -- write :: IO ()
 -- write =
